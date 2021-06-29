8bab23c64da732db0f8cf8ec9d3ad08ecd6dc46d
rayweathers88 <rayweathers88@gmail.com>
2021-06-29 03:41:23 +0300

    first commit

---
+++ Repositories/InMemItemsRepository.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using Catalog.Entities;
+
+namespace Catalog.Repositories
+{
+    public class InMemItemsRepository
+    {
+        private readonly List<Item> items = new()
+        {
+            
+        }
+    }
+}
